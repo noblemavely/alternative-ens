07722f133db089f129d325f54e88bdd88b585c39
Noble Mavely <noblemavely@Nobles-MacBook-Air-M1.local>
2026-04-12 08:43:02 +0530

    Clean up test files

--- test-resume-extract.ts
+++
@@ -1,8 +0,0 @@
-import { extractTextFromPDF } from "./server/resume-parser";
-import { readFileSync } from "fs";
-
-const resumePath = "/Users/noblemavely/Downloads/Resume - Noble Mavely - 2025 (1).pdf";
-const buffer = readFileSync(resumePath);
-
-const text = await extractTextFromPDF(buffer);
-console.log(text);
